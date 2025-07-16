08e88f2f84fd80ca417a072adc3f3b1acc147257
honhimW <honhimw@outlook.com>
2025-07-16 10:52:52 +0800

    MSRV 1.61

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!("cargo::rustc-check-cfg=cfg(nightly)");
-    let meta = rustc_version::version_meta().unwrap();
-    if meta.channel == rustc_version::Channel::Nightly {
-        // println!("cargo:rustc-cfg=rustc_nightly=true");
-        println!("cargo:rustc-cfg=nightly");
-    }
+    // println!("cargo::rustc-check-cfg=cfg(nightly)");
+    // let meta = rustc_version::version_meta().unwrap();
+    // if meta.channel == rustc_version::Channel::Nightly {
+    //     // println!("cargo:rustc-cfg=rustc_nightly=true");
+    //     println!("cargo:rustc-cfg=nightly");
+    // }
 }
--- src/nightly.rs
+++ src/nightly.rs
@@ -1,18 +1,18 @@
-#[cfg(test)]
-#[cfg(nightly)]
-mod test {
-
-    #[derive(Debug)]
-    struct Foo<'a> {
-        a: i32 = 1,
-        t: &'a str = "hello",
-    }
-
-    #[test]
-    fn new() {
-        let foo = Foo { .. };
-        assert_eq!(foo.a, 1);
-        assert_eq!(foo.t, "hello");
-    }
-
-}
+// #[cfg(test)]
+// #[cfg(nightly)]
+// mod test {
+// 
+//     #[derive(Debug)]
+//     struct Foo<'a> {
+//         a: i32 = 1,
+//         t: &'a str = "hello",
+//     }
+// 
+//     #[test]
+//     fn new() {
+//         let foo = Foo { .. };
+//         assert_eq!(foo.a, 1);
+//         assert_eq!(foo.t, "hello");
+//     }
+// 
+// }
